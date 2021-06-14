a2e76b859cb7e58ce55611aa88101f707264b1a3
csv-hezrontan <84384687+csv-hezrontan@users.noreply.github.com>
2021-06-14 18:28:02 +0800

    Added initial framework
    Added base test and page model

---
+++ BasicSelenium/Helper/DataEntryHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSelenium.Helper
+{
+    public static class DataEntryHelper
+    {
+        public static string GenerateId()
+        {
+            return Guid.NewGuid().ToString("N").ToUpper().Substring(1,10);
+        }
+
+        public static int GenerateNumber(int maxNumber = 100)
+        {
+            return new Random().Next(1, maxNumber);
+        }
+    }
+}
